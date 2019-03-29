e2d62d3bd39adc0e91abac1ba351826ad35863f8
Katty2203 <44648577+Katty2203@users.noreply.github.com>
2019-03-29 18:53:45 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,5 @@
+
+var d1=234245645335;
+var d2=3600;
+a= alert(d1 % d2);
+document.write(a);
---
+++ 2.js
@@ -0,0 +1,16 @@
+var n= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at.";
+var arr=[];
+for ( var i=0; (i<n.length); i++ ) 
+{
+if (n[i]=='a') {
+	arr.push(i);
+}
+}
+document.write(arr);
+
+
+
+
+	
+
+
